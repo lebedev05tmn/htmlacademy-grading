f61c5cf1d6d2ed110c973538566c23db49b1d82a
lebedev05tmn <lebedev05tmn@gmail.com>
2024-03-27 00:12:39 +0300

    1.3 adding styles

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 import "./slider";
+
+const map = L.map("map").setView([59.968322, 30.317359], 16);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const icon = L.icon({
+  iconUrl: "../images/icons/pin.svg",
+  iconSize: [38, 50],
+});
+
+const marker = L.marker([59.968322, 30.317359], { icon }).addTo(map);
